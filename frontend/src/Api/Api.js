f591a7873cd1f996208291f5932aa8c9e129c9cc
Suraj Poddar <surajku2022@gmail.com>
2023-08-19 22:09:14 +0530

    bugs fixed

--- frontend/src/Api/Api.js
+++ frontend/src/Api/Api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://care-for-you-backend.onrender.com",
+});
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.Authorization = `Bearer ${
@@ -10,51 +12,38 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
-axios.interceptors.response.use(
-  (response) => {
-    // Do something with the response data
-    return response;
-  },
-  (error) => {
-    // Do something with the response error
-    if (error.response.status === 401) {
-      localStorage.removeItem("profile");
-    }
-    return Promise.reject(error);
-  }
-);
 
-const baseURL = "http://localhost:5000";
+// const baseURL = "https://care-for-you-backend.onrender.com/";
 
 export const registerUser = (authData) => {
-  return API.post(`${baseURL}/user/register`, authData);
+  return API.post(`/user/register`, authData);
 };
 
 export const loginUser = (authData) => {
-  return API.post(`${baseURL}/user/login`, authData);
+  return API.post(`/user/login`, authData);
 };
 
 export const fetchWardData = () => {
-  return API.get(`${baseURL}/data`);
+  return API.get(`/data`);
 };
 
 export const postWardData = (wardData) => {
-  return API.post(`${baseURL}/data/addWard`, wardData);
+  return API.post(`/data/addWard`, wardData);
 };
 
 export const editWardData = (id, updatedData) => {
-  return API.patch(`${baseURL}/data/editWard/${id}`, updatedData);
+  return API.patch(`/data/editWard/${id}`, updatedData);
 };
 
 export const fetchReview = () => {
-  return API.get(`${baseURL}/reviews`);
+  return API.get(`/reviews`);
 };
 
 export const postReview = (review) => {
   console.log("Review Posted");
-  return API.post(`${baseURL}/reviews/`, review);
+  return API.post(`/reviews/`, review);
 };
 
 export const upvoteReview = (id) => {
-  return API.patch(`${baseURL}/reviews/${id}/vote`);
+  return API.patch(`/reviews/${id}/vote`);
 };
